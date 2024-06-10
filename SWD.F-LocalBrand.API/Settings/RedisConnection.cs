8c46db26109ba1a5a73a88cba02ec3252b2cd973
Trường Thịnh <thinhntse170224@fpt.edu.vn>
2024-06-11 00:53:05 +0700

    redis-mail-realtime config

---
+++ SWD.F-LocalBrand.API/Settings/RedisConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace SWD.F_LocalBrand.API.Settings
+{
+    public class RedisConnection
+    {
+        public string Port { get; set; }
+        public string Host { get; set; }
+        public string Password { get; set; }
+        public bool Enable { get; set; }
+    }
+}
