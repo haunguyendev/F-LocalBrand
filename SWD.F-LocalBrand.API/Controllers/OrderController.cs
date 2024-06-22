0c4b8b4000505f474c7fd0d313927ae778cb6d33
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-06-22 16:20:23 +0700

    [feat] Implement API to update order status
    - Added UpdateOrderStatusRequest class for request validation and mapping.
    - Created OrderStatusType static class for order status constants.
    - Implemented UpdateOrderStatus endpoint in OrderController to handle status updates.
    - Added IOrderService interface and OrderService class for business logic.
    - Added corresponding DTO and mapping logic in business layer.
    - Updated API documentation with Swagger annotations.

--- SWD.F-LocalBrand.API/Controllers/OrderController.cs
+++ SWD.F-LocalBrand.API/Controllers/OrderController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SWD.F_LocalBrand.API.Common;
+using SWD.F_LocalBrand.API.Payloads.Requests.Order;
 using SWD.F_LocalBrand.API.Payloads.Responses;
+using SWD.F_LocalBrand.Business.DTO.Order;
 using SWD.F_LocalBrand.Business.Services;
 
 namespace SWD.F_LocalBrand.API.Controllers
@@ -52,5 +54,41 @@ namespace SWD.F_LocalBrand.API.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+        #region api order status
+        [HttpPut("update-status")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(ApiResult<object>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(ApiResult<ValidationProblemDetails>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(ApiResult<object>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof(ApiResult<object>))]
+        public IActionResult UpdateOrderStatus([FromBody] UpdateOrderStatusRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ApiResult<ValidationProblemDetails>.Error(new ValidationProblemDetails(ModelState)));
+            }
+
+            try
+            {
+                var updateModel = new UpdateOrderStatusModel
+                {
+                    Id = request.Id,
+                    OrderStatus = request.OrderStatus
+                };
+
+                var updateResult = _orderService.UpdateOrderStatus(updateModel);
+
+                if (updateResult == null)
+                {
+                    return NotFound(ApiResult<object>.Error(new { Message = "Order not found" }));
+                }
+
+                return Ok(ApiResult<object>.Succeed(new { Message = "Order status updated successfully" }));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ApiResult<object>.Fail(ex));
+            }
+        }
+        #endregion
     }
 }
