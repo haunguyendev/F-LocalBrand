4d4688e653a2c37c875af5cda366d19dd36ffe30
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-06-22 13:35:35 +0700

    [feat] Add API for updating Category
    - Implemented UpdateCategoryAsync method in CategoryService
    - Added CategoryUpdateRequest and mapping logic
    - Updated CategoryController to support PUT method for category updates

---
+++ SWD.F-LocalBrand.API/Payloads/Requests/Category/CategoryUpdateRequest.cs
@@ -0,0 +1,27 @@
+﻿using SWD.F_LocalBrand.Business.DTO.Category;
+using System.ComponentModel.DataAnnotations;
+
+namespace SWD.F_LocalBrand.API.Payloads.Requests.Category
+{
+    public class CategoryUpdateRequest
+    {
+        [Required(ErrorMessage = "Id is required.")]
+        public int Id { get; set; }
+
+        [StringLength(255, ErrorMessage = "CategoryName must not exceed 255 characters.")]
+        public string CategoryName { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description must not exceed 500 characters.")]
+        public string Description { get; set; }
+
+        public CategoryUpdateModel MapToModel()
+        {
+            return new CategoryUpdateModel
+            {
+                Id = Id,
+                CategoryName = this.CategoryName,
+                Description = this.Description
+            };
+        }
+    }
+}
