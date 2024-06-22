dad1b10932baa81b3ef679c9ee6fe7498ed485bb
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-06-22 17:19:09 +0700

    [feat] Implement API to update collection details
    - Added CollectionUpdateRequest to handle update requests.
    - Created CollectionUpdateModel for business logic.
    - Updated CollectionService to support updating collection details, including optional Campaign and CollectionProducts.
    - Modified CollectionController to handle update endpoint and return appropriate responses.
    - Enhanced API documentation with Swagger annotations.

---
+++ SWD.F-LocalBrand.API/Payloads/Requests/Collection/CollectionUpdateRequest.cs
@@ -0,0 +1,28 @@
+﻿using SWD.F_LocalBrand.Business.DTO.Collection;
+using System.ComponentModel.DataAnnotations;
+
+namespace SWD.F_LocalBrand.API.Payloads.Requests.Collection
+{
+    public class CollectionUpdateRequest
+    {
+        [Required(ErrorMessage = "Id is required.")]
+        public int Id { get; set; }
+
+        public string? CollectionName { get; set; }
+
+        public int? CampaignId { get; set; }
+
+        public List<int>? CollectionProductIds { get; set; }
+
+        public CollectionUpdateModel MapToModel()
+        {
+            return new CollectionUpdateModel
+            {
+                Id = Id,
+                CollectionName = CollectionName,
+                CampaignId = CampaignId,
+                CollectionProductIds = CollectionProductIds
+            };
+        }
+    }
+}
