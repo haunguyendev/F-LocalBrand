b7f9ead59ecd8259cd8eca171e8bbdeb153f2c5e
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-06-23 13:18:14 +0700

    feat: Implement API to create new campaign

---
+++ SWD.F-LocalBrand.Services/DTO/Campaign/CampaignCreateModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.F_LocalBrand.Business.DTO.Campaign
+{
+    public class CampaignCreateModel
+    {
+        public string? CampaignName { get; set; }
+    }
+}
