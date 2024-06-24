4514e32bc8468ef9ee4ef89afb196b5938e3bbaa
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-06-24 20:33:57 +0700

    feat: Implement Create Payment API
    - Added endpoint to create a new payment record.
    - Validates input data for required fields: orderId, paymentDate, paymentMethod, paymentStatus, and statusResponseCode.
    - Returns appropriate response codes and messages for success and error scenarios.

---
+++ SWD.F-LocalBrand.API/Controllers/PaymentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using SWD.F_LocalBrand.API.Common;
+using SWD.F_LocalBrand.API.Payloads.Requests.Payment;
+using SWD.F_LocalBrand.Business.Services;
+using SWD.F_LocalBrand.Data.Models;
+
+namespace SWD.F_LocalBrand.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly PaymentService _paymentService;
+        public PaymentController(PaymentService paymentService)
+        {
+            _paymentService= paymentService;
+            
+        }
+
+        [HttpPost("create")]
+        [SwaggerOperation(
+       Summary = "Create a new payment",
+       Description = "Creates a new payment with the provided details."
+   )]
+        [SwaggerResponse(StatusCodes.Status200OK, "Payment created successfully", typeof(ApiResult<Payment>))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request", typeof(ApiResult<Dictionary<string, string[]>>))]
+        [SwaggerResponse(StatusCodes.Status500InternalServerError, "An error occurred while creating the payment", typeof(ApiResult<object>))]
+        public async Task<IActionResult> CreatePayment([FromBody] PaymentCreateRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(v => v.Errors)
+                                               .Select(e => e.ErrorMessage)
+                                               .ToList();
+                return BadRequest(ApiResult<Dictionary<string, string[]>>.Error(new Dictionary<string, string[]>
+            {
+                { "Errors", errors.ToArray() }
+            }));
+            }
+
+            try
+            {
+                var paymentModel = request.MapToModel();
+                var payment = await _paymentService.CreatePaymentAsync(paymentModel);
+                return Ok(ApiResult<Payment>.Succeed(payment));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ApiResult<object>.Fail(ex));
+            }
+        }
+
+    }
+}
