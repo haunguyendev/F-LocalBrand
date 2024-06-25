aac70d22a0fd0b94d46ec8cc98f8e6f956ae3331
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-06-25 11:46:13 +0700

    feat: Implement Customer Service Update Methods

---
+++ SWD.F-LocalBrand.Services/DTO/Customer/CustomerUpdateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.F_LocalBrand.Business.DTO.Customer
+{
+    public class CustomerUpdateModel
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? Image { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+    }
+}
--- SWD.F-LocalBrand.Services/Services/IdentityService.cs
+++ SWD.F-LocalBrand.Services/Services/IdentityService.cs
@@ -262,24 +262,28 @@ public class IdentityService
                 ApplicationName = "F-LocalBrand",
             });
             Userinfo userInfo = await oauth2Service.Userinfo.Get().ExecuteAsync();
-            var user = await _unitOfWork.Users.FindByCondition(u => u.Email == userInfo.Email).FirstOrDefaultAsync();
+            var user = await _unitOfWork.Customers.FindByCondition(u => u.Email == userInfo.Email).FirstOrDefaultAsync();
             if (user == null)
             {
-                user = new User()
+                user = new Customer()
                 {
                     Email = userInfo.Email,
-                    UserName = userInfo.Name,
-                    RoleId = 1,
+                    FullName=userInfo.Name,
+                    Image=user?.Image,
+                    Phone=user.Phone,
+                    Address=user.Address,
+
+                    
                     RegistrationDate = DateOnly.FromDateTime(DateTime.Now)
 
 
                 };
-                await _unitOfWork.Users.CreateAsync(user);
+                await _unitOfWork.Customers.CreateAsync(user);
                 await _unitOfWork.CommitAsync();
             }
 
-            var tokenResponse = CreateJwtToken(user);
-            var tokenRefreshResponse = CreateJwtRefreshToken(user);
+            var tokenResponse = CreateJwtTokenCustomer(user);
+            var tokenRefreshResponse = CreateJwtRefreshTokenCustomer(user);
             return new LoginResult
             {
                 Authenticated = true,
