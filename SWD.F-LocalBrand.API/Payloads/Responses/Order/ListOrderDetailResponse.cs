f0727d9cd9320e3cbdf15c50e74df4caa0537429
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-07-17 19:36:27 +0700

    [feat] api get details of order

---
+++ SWD.F-LocalBrand.API/Payloads/Responses/Order/ListOrderDetailResponse.cs
@@ -0,0 +1,9 @@
+﻿using SWD.F_LocalBrand.Business.DTO.Order;
+
+namespace SWD.F_LocalBrand.API.Payloads.Responses.Order
+{
+    public class ListOrderDetailResponse
+    {
+        public List<OrderDetailResponseModel> Details { get; set; }
+    }
+}
--- SWD.F-LocalBrand.Data/Common/Interfaces/IOrderDetailRepository.cs
+++ SWD.F-LocalBrand.Data/Common/Interfaces/IOrderDetailRepository.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +10,6 @@ namespace SWD.F_LocalBrand.Data.Common.Interfaces
 {
     public interface IOrderDetailRepository : IRepositoryBaseAsync<OrderDetail>
     {
+        Task<List<OrderDetail>?> FindOrderDetailAsync(Expression<Func<OrderDetail, bool>> predicate);
     }
 }
