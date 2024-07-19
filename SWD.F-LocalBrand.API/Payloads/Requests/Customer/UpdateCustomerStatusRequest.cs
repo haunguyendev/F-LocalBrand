67eafa5a615c7d45ac6ccef538357c94c7376418
Nguyễn Trung Hậu <haunt150603@gmail.com>
2024-07-19 17:07:17 +0700

    [fix] update

--- SWD.F-LocalBrand.API/Payloads/Requests/Customer/UpdateCustomerStatusRequest.cs
+++ SWD.F-LocalBrand.API/Payloads/Requests/Customer/UpdateCustomerStatusRequest.cs
@@ -4,7 +4,8 @@ namespace SWD.F_LocalBrand.API.Payloads.Requests.Customer
 {
     public class UpdateCustomerStatusRequest
     {
-       
+        [Required]
+        public int CustomerId {  get; set; }
         [Required]
         [RegularExpression("^(Active|Inactive)$", ErrorMessage = "Status must be 'Active' or 'Inactive'.")]
         public string Status { get; set; }
